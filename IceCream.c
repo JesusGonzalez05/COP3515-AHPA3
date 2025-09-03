2c43dab4f0ca4bf3077a8cf360590067a07ffbfd
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-09-03 18:05:36 -0400

    added corner1Sales, corner2Sales and totalSales array as well as Bob’s entered sales data.

--- IceCream.c
+++ IceCream.c
@@ -11,12 +11,16 @@ Student Name: Jesus Gonzalez
 enum flavors {vanilla, chocolate, mint, strawberry};
 
 int main(void) {
+    double corner1Sales[4] = {0};
+    double corner2Sales[4] = {0};
+    double totalSales[4] = {0};
 //Create a program that allows a user to enter the type of ice cream sold
 
 //Create a program that allows a user to enter the sales data.
 
 //Load two one-dimensional arrays with Bob’s entered sales data. Only index your arrays using an enum variable (enum flavors index = vanilla; corner1Sales[index] = 11.5;)
-
+    double corner1Sales[4] = {11.5, 15.75, 22.0, 7.25};
+    double corner2Sales[4]   = {12.75, 9.25, 16.5, 21.0};
 //Sum the contents of the flavors in the arrays together.
 
 //Print out the resulting flavors and their sum with Bob's total sales.
