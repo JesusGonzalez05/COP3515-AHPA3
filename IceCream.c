1486899980dae5d9ac5d0c6f1fbd3aae545cdabc
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-09-03 17:44:36 -0400

    added enum for flavor and broke down the needed code via comments in main function.

--- IceCream.c
+++ IceCream.c
@@ -6,6 +6,21 @@ Student Name: Jesus Gonzalez
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
+
+// create enums for the flavors
+enum flavors {vanilla, chocolate, mint, strawberry};
+
 int main(void) {
+//Create a program that allows a user to enter the type of ice cream sold
+
+//Create a program that allows a user to enter the sales data.
+
+//Load two one-dimensional arrays with Bob’s entered sales data. Only index your arrays using an enum variable (enum flavors index = vanilla; corner1Sales[index] = 11.5;)
+
+//Sum the contents of the flavors in the arrays together.
+
+//Print out the resulting flavors and their sum with Bob's total sales.
+
+
 return 0;
 }
